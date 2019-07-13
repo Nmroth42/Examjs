d84fa80b4ec06d1de2b03ebbcb21fe26b0b38159
Nmroth42 <mannoroth42@gmail.com>
2019-07-13 10:44:48 +0700

    dep6

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,6 +2,9 @@ import axios from 'axios'
 
 export default () => {
     return axios.create({
-      baseURL: `https://examjs.herokuapp.com/`
+    
+      baseURL: `https://examjs.herokuapp.com`
+      // baseURL: `http://localhost:8080/`
+      //  baseURL: `https://examjs.herokuapp.com/`
     })
   }
