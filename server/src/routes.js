63bb626890d6c6cf49f5211cc4ec0271dfd17370
Nmroth42 <mannoroth42@gmail.com>
2019-07-13 11:15:40 +0700

    dep10

--- server/src/routes.js
+++ server/src/routes.js
@@ -2,13 +2,13 @@ const TestController = require("../controllers/testServices");
 
 module.exports = app => {
   //описание тестов
-  app.get("examjs.herokuapp.com/api/tests", TestController.AllTestTitles);
+  app.get("/api/tests", TestController.AllTestTitles);
   //описание отдельного теста
-  app.get("examjs.herokuapp.com/api/test/:testId", TestController.oneTestTitle);
+  app.get("/api/test/:testId", TestController.oneTestTitle);
 
   //содержание теста(не обязательно привязанному по id)
-  app.get("examjs.herokuapp.com/api/questions/:testId", TestController.testContent);
+  app.get("/api/questions/:testId", TestController.testContent);
 
   //результат одного теста
-  app.post("examjs.herokuapp.com/api/answers", TestController.resultTest);
+  app.post("/api/answers", TestController.resultTest);
 };
